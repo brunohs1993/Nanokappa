e9f014df8525e7332893f28ac417b7903135e124
Bruno Hartmann <brunohs1993@gmail.com>
2022-11-17 12:09:43 +0100

    run_csv.py functional locally and on cluster.

--- optimiser/run_csv.py
+++ optimiser/run_csv.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import argparse
 from multiprocessing import Pool
 from functools import partial
@@ -20,17 +19,26 @@ params_db = import_params(csv_file)
 
 N_sims = len(params_db.index)
 
+##### THESE PARAMETERS SHOULD BE PARSED FROM SOME FILE. THEY ARE HERE FOR TESTING #####
+
+# params to be defined if running on cluster
 cluster_params = dict(mail_user  = 'bruno.hartmann-da-silva@univ-lorraine.fr',
                       job_name   = 'nanokappa',
-                      time       = 'calculate',
                       partition  = 'std',
                       n_of_nodes = 1,
                       n_of_tasks = 1,
-                      mail_type  = 'ALL')
+                      mail_type  = 'ALL',
+                      nanokappa_folder = '/home/dcc70/tmr51/Nanokappa',
+                      conda_env = 'nanokappa')
 
+# params to be defined if running locally
+# these are much less because the user would be able to change the csv
+# accordingly, while in the cluster it may be more difficult to do
 local_params = dict(processes = 3,
                     nanokappa_path = 'd:/LEMTA/Code',
                     nanokappa_folder = 'Nanokappa')
+
+#######################################################
 
 if mode == 'cluster':
     for i in range(N_sims):
