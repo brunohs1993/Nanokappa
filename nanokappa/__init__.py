c9157520a633140e731d8b8954764ab97636318b
brunohs1993 <brunohartmann.silva@gmail.com>
2024-11-01 19:25:57 +0100

    Added init files

---
+++ nanokappa/__init__.py
@@ -0,0 +1,2 @@
+# Copyright (C) 2024, Bruno Hartmann da Silva
+# License: MIT
---
+++ nanokappa/geometry/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2024, Bruno Hartmann da Silva
+# License: MIT
+
+from nanokappa.geometry.mesh import Mesh
+from nanokappa.geometry.subvol_classifier import SubvolClassifier
+
+
+__all__ = ["Mesh", "SubvolClassifier"]
