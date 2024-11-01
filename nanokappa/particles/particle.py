c0ae52a4251a4f8076cda4a02b6601688c0708be
brunohs1993 <brunohartmann.silva@gmail.com>
2024-11-01 20:15:01 +0100

    Developed particles a bit more.

--- nanokappa/particles/particle.py
+++ nanokappa/particles/particle.py
@@ -2,8 +2,18 @@
 # License: MIT
 
 import numpy as np
+from pydantic import BaseModel
 
 
-class Particle:
-    def __init__(self):
-        self.position = np.zeros((3))
+class Particle(BaseModel):
+    position: np.ndarray
+    velocity: np.ndarray
+
+    def move(self, dt):
+        """Move the particle one timestep.
+
+        Parameters
+        ----------
+        dt: The time step in seconds.
+        """
+        self.position = self.velocity * dt
--- nanokappa/particles/phonon.py
+++ nanokappa/particles/phonon.py
@@ -1,2 +1,21 @@
+# Copyright (C) 2024, Bruno Hartmann da Silva
+# License: MIT
+
+import numpy as np
+from types import FunctionType
+from nanokappa.particles import Particle
+from nanokappa.utils import Constants as C
+
+
 class Phonon(Particle):
-    pass
+    omega: float
+    occupation: float
+    tau_function: FunctionType
+
+    def scatter(self, T: float):
+        n0 = self.occupation_function(T)
+        tau = self.tau_function(T)
+        self.occupation = n0 + (self.occupation - n0) * np.exp(-self.dt / tau)
+
+    def occupation_function(self, T):
+        return 0.5 + 1 / (np.exp(C.hbar * self.omega / (C.kb * T)) - 1)
