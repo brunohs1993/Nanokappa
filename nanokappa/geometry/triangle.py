cd66aaa0daf7eb736e8fba9e069e97ac93f2350e
brunohs1993 <brunohartmann.silva@gmail.com>
2024-11-03 13:01:57 +0100

    Set Triangle class basis

--- nanokappa/geometry/triangle.py
+++ nanokappa/geometry/triangle.py
@@ -1,30 +1,40 @@
-# Copyright (C) 2024, Bruno Hartmann da Silva
+# Copyright (C) 2023-2024, Bruno Hartmann da Silva
 # License: MIT
 
 import numpy as np
-from pydantic import BaseModel
-from nanokappa.geometry import Vector3D, TriVectors
+from pydantic import computed_field
+from functools import cached_property
+from nanokappa.base import BaseModel, TriVectors, Vector3D
 
 
-class Triangle(BaseModel):
-    vertices : TriVectors
+class Triangle(BaseModel, frozen=True):
+    vertices: TriVectors
 
-    def __init__(self):
-        super.__init__()
-        self.origin = self.vertices[0]
-        self.normal = self.get_normal()
-        self.basis = self.get_basis()
-        self.plane_k = self.get_plane_k()
+    @computed_field
+    @cached_property
+    def origin(self) -> Vector3D:
+        return self.vertices[0]
 
-    def get_basis(self) -> TriVectors:
+    @computed_field
+    @cached_property
+    def basis(self) -> TriVectors:
         b1 = self.vertices[1] - self.origin
         b2 = self.vertices[2] - self.origin
-        return np.vstack(self.normal, b1, b2)
+        return np.vstack((self.normal, b1, b2))
 
-    def get_normal(self) -> Vector3D:
+    @computed_field
+    @cached_property
+    def normal(self) -> Vector3D:
         v1 = self.vertices[1] - self.origin
         v2 = self.vertices[2] - self.origin
         return np.cross(v1, v2)
 
-    def get_plane_k(self) -> float:
+    @computed_field
+    @cached_property
+    def plane_k(self) -> float:
         return -np.sum(self.normal * self.origin)
+
+    @computed_field
+    @cached_property
+    def bounds(self) -> float:
+        return np.vstack((self.vertices.min(axis=0), self.vertices.max(axis=0)))
